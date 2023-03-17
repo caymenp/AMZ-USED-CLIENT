2dd33f6b749366d39a430beda7d6a4643d3c6f0c
caymenp <109010237+caymenp@users.noreply.github.com>
2023-03-17 15:45:15 -0500

    Update AdsBanner.js

--- amz-client/src/components/AdsBanner.js
+++ amz-client/src/components/AdsBanner.js
@@ -1,32 +1,17 @@
 import React, { useEffect, useRef, useState } from "react";
 
-export const AdterraBanner = () => {
-  const [width, setWidth] = useState(window.innerWidth);
-  const breakPoint = 730;
-
+export const MobileAd = () => {
   const banner = useRef();
 
   let atOptions = {
-    key: "",
+    key: "47eeffc47431a6c4c6a0eecdd3ecc57c",
     format: "iframe",
-    height: 0,
-    width: 0,
+    height: 50,
+    width: 320,
     params: {},
   };
 
   useEffect(() => {
-    window.addEventListener("resize", () => setWidth(window.innerWidth));
-
-    if (width < breakPoint) {
-      atOptions.key = "47eeffc47431a6c4c6a0eecdd3ecc57c";
-      atOptions.height = 50;
-      atOptions.width = 320;
-    } else {
-      atOptions.key = "4c5581218a5c987490d5ea466bd596e7";
-      atOptions.height = 90;
-      atOptions.width = 728;
-    }
-
     if (!banner.current.firstChild) {
       const conf = document.createElement("script");
       const script = document.createElement("script");
@@ -42,4 +27,44 @@ export const AdterraBanner = () => {
   }, []);
 
   return <div style={{ textAlign: "center" }} ref={banner} />;
+};
+
+export const DesktopAd = () => {
+  const banner = useRef();
+
+  let atOptions = {
+    key: "4c5581218a5c987490d5ea466bd596e7",
+    format: "iframe",
+    height: 90,
+    width: 728,
+    params: {},
+  };
+
+  useEffect(() => {
+    if (!banner.current.firstChild) {
+      const conf = document.createElement("script");
+      const script = document.createElement("script");
+      script.type = "text/javascript";
+      script.src = `//www.profitabledisplaynetwork.com/${atOptions.key}/invoke.js`;
+      conf.innerHTML = `atOptions = ${JSON.stringify(atOptions)}`;
+
+      if (banner.current) {
+        banner.current.append(conf);
+        banner.current.append(script);
+      }
+    }
+  }, []);
+
+  return <div style={{ textAlign: "center" }} ref={banner} />;
+};
+
+export const AdterraBanner = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+  const breakPoint = 730;
+
+  useEffect(() => {
+    window.addEventListener("resize", () => setWidth(window.innerWidth));
+  }, []);
+
+  return width < breakPoint ? <MobileAd /> : <DesktopAd />;
 };
